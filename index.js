17bb156de57df49af3300db8f53273aa91ee9e15
jahirul94 <jahirulislam41185@gmail.com>
2023-04-30 15:43:45 +0600

    server complate

--- index.js
+++ index.js
@@ -3,12 +3,34 @@ const app = express();
 const port = 5000 ;
 const cors = require('cors');
 const catagories = require('./data/catagorys.json')
+const news = require ('./data/news.json')
 
 app.use(cors());
 
 app.get('/', ( req , res )=>{
     res.send('Dragon is Running')
 
+})
+
+app.get('/news',(req , res ) =>{
+    res.send(news)
+})
+
+app.get('/news/:id' , (req , res )=>{
+   const id = req.params.id;
+   const selectedNews = news.find(n => n._id === id)
+   res.send(selectedNews)
+})
+
+app.get('/catagories/:id',(req , res )=>{
+    const id = req.params.id;
+   if(id == 0){
+      res.send(news)
+   }
+   else {
+       const selectedNews = news.filter(n => n.category_id === id)
+        res.send(selectedNews)
+   }
 })
 
 app.get('/catagories', (req,res)=>{
